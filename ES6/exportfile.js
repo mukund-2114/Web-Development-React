3e5e9e8959a412a33d94c74b9c4f37ec3941c091
mukund2114 <mdrkkapadia@gmail.com>
2023-03-11 22:11:35 +0530

    Javascript files added

---
+++ ES6/exportfile.js
@@ -0,0 +1,7 @@
+export const addition = (x,y) => {
+    return x+y;
+};
+
+export const subtract = (x,y) => {
+    return x-y;
+};
---
+++ ES6/importfile.js
@@ -0,0 +1,4 @@
+import {addition,subtract} from './exportfile.js';
+
+console.log(addition(5,6));
+console.log(subtract(10,8));
